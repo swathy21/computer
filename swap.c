f588f62cfe6d52c83a889d1949ad5e959ea1783f
swathy21 <42170034+swathy21@users.noreply.github.com>
2019-07-30 16:41:09 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+void main(){
+     char str[20],tmp;
+     int i,j;
+    
+     scanf("%s",str);
+     for(i=0;i<strlen(str);i=i+2)
+     {
+     tmp = str[i];
+     str[i] = str[i+1];
+     str[i+1] = tmp;
+     }
+     printf( "%s",str);
+     getch();
+}
